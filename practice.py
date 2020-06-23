4c0cb772f739ba5d161d1ce1561e03d4fa1d332c
mohammad yasin <66986310+mes-shahadat@users.noreply.github.com>
2020-06-23 07:44:51 -0700

    Delete practice.py

--- practice.py
+++
@@ -1,5 +0,0 @@
-print( "hi my name is mes \ni live in bangladesh in chittagong district \nchaktai, chittagong to be exact \nnow iam learning python cause i want to be an backend python developer.\ni am a student\\college student \t \"inter 2nd year\" ") # work hard mes
-print ( "mes no escape sequence code" r" \' \" \n \t ")
-print ( " emojie \U0001F929")
-name, age = input ("type your name and your age with coma in between") .split (",") # this is an input demo
-print ( f"welcome {name} your age is {age}")
