781cecd0e3df66c18c02cdd70eacb4124a4c2cdb
mes-shahadat <binodonprojukti@gmail.com>
2020-06-23 07:59:49 -0800

    test removed

--- hello.py
+++
@@ -1,2 +0,0 @@
-print ('hey mes how are you doing')
-print (' keep working hard mes you have to do something in this world ')
--- hello3.py
+++
@@ -1,2 +0,0 @@
-print (' work work work  keep working do not leave keep working ')
-print (" so you think you're a hero huh \n no you don't \n you fool\ediot")
